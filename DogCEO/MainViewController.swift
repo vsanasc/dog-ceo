b9e3e9c5ec7e136eb086d4b678965837db56a0b9
Vitor S. do Nascimento <vsantos.nasc@gmail.com>
2021-04-03 13:29:15 -0300

    Feat(main screen): Add list breed integration

--- DogCEO/MainViewController.swift
+++ DogCEO/MainViewController.swift
@@ -7,12 +7,99 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
+class MainViewController: UITableViewController {
 
+    lazy var loadingView: UIActivityIndicatorView = {
+        let view = UIActivityIndicatorView()
+        view.startAnimating()
+        view.style = .large
+        view.color = .black
+        return view
+    }()
+
+    let network = NetworkAPI()
+    var items: [String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        loadData()
+    }
+    func setupUI() {
+        title = "Dog CEO"
+        tableView.separatorStyle = .none
+    }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        items.count
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = items[indexPath.row].capitalized
+        return cell
+    }
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    func showLoading() {
+        view.addSubview(loadingView)
+        loadingView.centerParent()
+        UIView.animate(
+            withDuration: 0.3,
+            animations: {
+                self.loadingView.alpha = 1
+                self.tableView.separatorStyle = .none
+            }
+        )
+    }
+    func hideLoading() {
+        UIView.animate(
+            withDuration: 0.3,
+            animations: {
+                self.loadingView.alpha = 0
+                self.tableView.separatorStyle = .singleLine
+            }
+        )
+    }
+
+    func loadData() {
+        showLoading()
+        network.getBreedsList(
+            onSuccess: { [weak self] response in
+                if let self = self {
+                    self.items = response.items
+                    DispatchQueue.main.async {
+                        self.hideLoading()
+                        self.tableView.reloadData()
+                    }
+                }
+            },
+            onError: { error in
+                DispatchQueue.main.async {
+                    self.showErrorMessage()
+                }
+            }
+        )
+    }
+
+    func showErrorMessage() {
+        let alert = UIAlertController(
+            title: "Error de Servicio",
+            message: "The service isn't available",
+            preferredStyle: .alert
+        )
+        alert.addAction(
+            UIAlertAction(
+                title: "Retry",
+                style: .default,
+                handler: { action in
+                    self.loadData()
+                }
+            )
+        )
+        present(alert, animated: true)
     }
 
 }
