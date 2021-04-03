6e28bf561212657f5d3049eb1638fb897e232e45
Vitor S. do Nascimento <vsantos.nasc@gmail.com>
2021-04-03 18:51:46 -0300

    Refactor(BreedImages): Apply BaseNetwork

--- DogCEO/Controllers/BreedImagesViewController.swift
+++ DogCEO/Controllers/BreedImagesViewController.swift
@@ -7,15 +7,11 @@
 
 import UIKit
 
-class BreedImagesViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class BreedImagesViewController: BaseNetworkViewController,
+UICollectionViewDelegate,
+UICollectionViewDataSource,
+UICollectionViewDelegateFlowLayout {
 
-    lazy var loadingView: UIActivityIndicatorView = {
-        let view = UIActivityIndicatorView()
-        view.startAnimating()
-        view.style = .large
-        view.color = .black
-        return view
-    }()
     lazy var collectionView: UICollectionView = {
         let view = UICollectionView(frame: .zero, collectionViewLayout: createViewLayout())
         view.delegate = self
@@ -24,68 +20,21 @@ class BreedImagesViewController: UIViewController, UICollectionViewDelegate, UIC
         view.backgroundColor = .white
         return view
     }()
-    let network = NetworkAPI()
     var breedSelected = ""
-    var images: [String] = []
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupUI()
-        loadData()
-    }
-    func setupUI() {
+
+    override func setupUI() {
+        super.setupUI()
         title = breedSelected.capitalized
         view.addSubview(collectionView)
         collectionView.fillParent()
     }
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        images.count
-    }
-
-    func collectionView(
-        _ collectionView: UICollectionView,
-        cellForItemAt indexPath: IndexPath
-    ) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-        if let cell = cell as? ImageCell {
-            cell.setupData(url: images[indexPath.row])
-        }
-        return cell
-    }
-
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        collectionView.deselectItem(at: indexPath, animated: true)
-
-        let dest = ImagesPreviewViewController(images: images, selected: indexPath.row)
-        navigationController?.pushViewController(dest, animated: true)
-
-    }
-
-    func showLoading() {
-        view.addSubview(loadingView)
-        loadingView.centerParent()
-        UIView.animate(
-            withDuration: 0.3,
-            animations: {
-                self.loadingView.alpha = 1
-            }
-        )
-    }
-    func hideLoading() {
-        UIView.animate(
-            withDuration: 0.3,
-            animations: {
-                self.loadingView.alpha = 0
-            }
-        )
-    }
-
-    func loadData() {
-        showLoading()
+    override func loadData() {
+        super.loadData()
         network.getImagesList(
             breed: breedSelected,
             onSuccess: { [weak self] response in
                 if let self = self {
-                    self.images = response.images
+                    self.items = response.images
                     DispatchQueue.main.async {
                         self.hideLoading()
                         self.collectionView.reloadData()
@@ -99,26 +48,29 @@ class BreedImagesViewController: UIViewController, UICollectionViewDelegate, UIC
             }
         )
     }
-
-    func showErrorMessage() {
-        let alert = UIAlertController(
-            title: "Error de Servicio",
-            message: "The service isn't available",
-            preferredStyle: .alert
-        )
-        alert.addAction(
-            UIAlertAction(
-                title: "Retry",
-                style: .default,
-                handler: { action in
-                    self.loadData()
-                }
-            )
-        )
-        present(alert, animated: true)
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        items.count
     }
 
-    func createViewLayout() -> UICollectionViewCompositionalLayout {
+    func collectionView(
+        _ collectionView: UICollectionView,
+        cellForItemAt indexPath: IndexPath
+    ) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        if let cell = cell as? ImageCell {
+            cell.setupData(url: items[indexPath.row])
+        }
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+
+        let dest = ImagesPreviewViewController(images: items, selected: indexPath.row)
+        navigationController?.pushViewController(dest, animated: true)
+
+    }
+    private func createViewLayout() -> UICollectionViewCompositionalLayout {
         let item = NSCollectionLayoutItem(
             layoutSize: .init(
                 widthDimension: .fractionalWidth(1),
