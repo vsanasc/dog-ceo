209ea209f299f71af821b4c1a49dce5d819659f8
Vitor S. do Nascimento <vsantos.nasc@gmail.com>
2021-04-03 18:48:09 -0300

    Feat(commons): Add LoadingView

---
+++ DogCEO/Commons/LoadingView.swift
@@ -0,0 +1,48 @@
+//
+//  LoadingView.swift
+//  Dog CEO
+//
+//  Created by Vitor on 03-04-21.
+//
+
+import UIKit
+
+class LoadingView: UIView {
+
+    lazy var label: UILabel = {
+        let view = UILabel()
+        view.text = "cargando..."
+        view.font = .systemFont(ofSize: 16.0)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    lazy var activityView: UIActivityIndicatorView = {
+        let view = UIActivityIndicatorView()
+        view.startAnimating()
+        view.style = .large
+        view.color = .black
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        addSubviews()
+        setupConstraints()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    private func addSubviews() {
+        addSubview(activityView)
+        addSubview(label)
+    }
+    private func setupConstraints() {
+        activityView.centerParent()
+        let constraints: [NSLayoutConstraint] = [
+            label.topAnchor.constraint(equalTo: activityView.bottomAnchor, constant: 10),
+            label.centerXAnchor.constraint(equalTo: activityView.centerXAnchor)
+        ]
+        NSLayoutConstraint.activate(constraints)
+    }
+}
