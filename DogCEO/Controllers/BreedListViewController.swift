ff395108f27b4589d7f368dbf874510a125bb79b
Vitor S. do Nascimento <vsantos.nasc@gmail.com>
2021-04-03 18:52:38 -0300

    Refactor(BreedList): Apply BaseNetwork

--- DogCEO/Controllers/BreedListViewController.swift
+++ DogCEO/Controllers/BreedListViewController.swift
@@ -7,71 +7,23 @@
 
 import UIKit
 
-class BreedListViewController: UITableViewController {
+class BreedListViewController: BaseNetworkViewController, UITableViewDelegate, UITableViewDataSource {
 
-    lazy var loadingView: UIActivityIndicatorView = {
-        let view = UIActivityIndicatorView()
-        view.startAnimating()
-        view.style = .large
-        view.color = .black
+    lazy var tableView: UITableView = {
+        let view = UITableView()
+        view.delegate = self
+        view.dataSource = self
+        view.backgroundColor = .white
         return view
     }()
 
-    let network = NetworkAPI()
-    var items: [String] = []
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        setupUI()
-        loadData()
+    override func setupUI() {
+        super.setupUI()
+        view.addSubview(tableView)
+        tableView.fillParent()
     }
-    func setupUI() {
-        title = "Dog CEO"
-        tableView.separatorStyle = .none
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        items.count
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = items[indexPath.row].capitalized
-        return cell
-    }
-
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        tableView.deselectRow(at: indexPath, animated: true)
-        let dest = BreedImagesViewController()
-        dest.breedSelected = items[indexPath.row]
-        DispatchQueue.main.async {
-            self.navigationController?.pushViewController(dest, animated: true)
-        }
-
-    }
-
-    func showLoading() {
-        view.addSubview(loadingView)
-        loadingView.centerParent()
-        UIView.animate(
-            withDuration: 0.3,
-            animations: {
-                self.loadingView.alpha = 1
-                self.tableView.separatorStyle = .none
-            }
-        )
-    }
-    func hideLoading() {
-        UIView.animate(
-            withDuration: 0.3,
-            animations: {
-                self.loadingView.alpha = 0
-                self.tableView.separatorStyle = .singleLine
-            }
-        )
-    }
-
-    func loadData() {
-        showLoading()
+    override func loadData() {
+        super.loadData()
         network.getBreedsList(
             onSuccess: { [weak self] response in
                 if let self = self {
@@ -89,23 +41,23 @@ class BreedListViewController: UITableViewController {
             }
         )
     }
-
-    func showErrorMessage() {
-        let alert = UIAlertController(
-            title: "Error de Servicio",
-            message: "The service isn't available",
-            preferredStyle: .alert
-        )
-        alert.addAction(
-            UIAlertAction(
-                title: "Retry",
-                style: .default,
-                handler: { action in
-                    self.loadData()
-                }
-            )
-        )
-        present(alert, animated: true)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        items.count
     }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = items[indexPath.row].capitalized
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let dest = BreedImagesViewController()
+        dest.breedSelected = items[indexPath.row]
+        DispatchQueue.main.async {
+            self.navigationController?.pushViewController(dest, animated: true)
+        }
+
+    }
+
 
 }
