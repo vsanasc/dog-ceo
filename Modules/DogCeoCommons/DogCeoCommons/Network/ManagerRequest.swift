6c60137667062a72668d17d6ffc8d3bdedb05350
Vitor S. do Nascimento <vsantos.nasc@gmail.com>
2021-04-04 22:21:31 -0400

    Refactor(network): Remove Endpoint file

--- Modules/DogCeoCommons/DogCeoCommons/Network/ManagerRequest.swift
+++ Modules/DogCeoCommons/DogCeoCommons/Network/ManagerRequest.swift
@@ -5,6 +5,9 @@
 //  Created by Vitor on 03-04-21.
 //
 
+public typealias Parameters = [String: Any]
+public typealias HTTPHeaders = [String: String]
+
 public struct ManagerRequest {
     public let url: String
     public var method: NetworkHTTPMethod
